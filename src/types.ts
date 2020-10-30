334f59719c4f523eef3fb4b3e8ae918cf2e1b7ff
EvanJ0hnson <ivangerasimow@gmail.com>
2020-10-30 16:48:32 +0300

    feat(types) Implement "TRef" type

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,9 @@
+import type { MutableRefObject } from 'react';
 import * as React from 'react';
 
 export type WithReactElement<T> = T & { element: React.ReactElement };
+
+export type TRef<E> =
+  | ((instance: E | null) => void)
+  | MutableRefObject<E | null>
+  | null;
